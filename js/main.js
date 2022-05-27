9bdfda3ca01a5ea7fc81b8235a9900e29ad42991
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-05-28 00:15:22 +0300

    add slider-img

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+  $('.gallery__slider').slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img class="arrow arrow-prev" src="images/icon/arrow-prev.svg" alt="arrow-prev"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img class="arrow arrow-next" src="images/icon/arrow-next.svg" alt="arrow-next"></button>',
+  });
 });
