75224965a51485b895b74a1940fbb4ac3d661921
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-05-30 20:56:38 +0300

    add direction and info

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 $(function () {
   $('.gallery__slider').slick({
     prevArrow:
-      '<button type="button" class="slick-btn slick-prev"><img class="arrow arrow-prev" src="images/icon/arrow-prev.svg" alt="arrow-prev"></button>',
+      '<button type="button" class="slick-btn slick-prev"><svg class="arrow arrow-prev"  width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect x="43.5" y="43.5" width="43" height="43" rx="21.5" transform="rotate(-180 43.5 43.5)" stroke="#C4C4C4"/><path d="M24 29L18 23.0016L24 17" stroke="#white" stroke-width="2" stroke-miterlimit="10"/></svg></button>',
     nextArrow:
-      '<button type="button" class="slick-btn slick-next"><img class="arrow arrow-next" src="images/icon/arrow-next.svg" alt="arrow-next"></button>',
+      '<button type="button" class="slick-btn slick-next"><svg class="arrow arrow-next" width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.5" y="0.5" width="43" height="43" rx="21.5" stroke="#C4C4C4"/><path d="M19.2207 17L25.2207 22.9984L19.2207 29" stroke="#white" stroke-width="2" stroke-miterlimit="10"/></svg></button>',
+  });
+
+  $('.direction__inner').slick({
+    mobileFirst: true,
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><svg class="arrow arrow-prev arrow--direction-prev"  width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect x="43.5" y="43.5" width="43" height="43" rx="21.5" transform="rotate(-180 43.5 43.5)" stroke="#886239"/><path d="M24 29L18 23.0016L24 17" stroke="#886239" stroke-width="2" stroke-miterlimit="10"/></svg></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><svg class="arrow arrow-next arrow--direction-next" width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.5" y="0.5" width="43" height="43" rx="21.5" stroke="#886239"/><path d="M19.2207 17L25.2207 22.9984L19.2207 29" stroke="#886239" stroke-width="2" stroke-miterlimit="10"/></svg></button>',
+    responsive: [
+      {
+        breakpoint: 601,
+        settings: 'unslick',
+      },
+    ],
   });
 
   $('.gallery__item-box').magnificPopup({
